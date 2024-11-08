bc1f39ebffa0e886c2efbdab7337ea0b7dd5c3b6
Tahir Khan <tahirpmsss.hitcse2020@gmail.com>
2024-11-08 16:14:28 +0530

    made changes

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -8,7 +8,7 @@
         @foreach ($jobs as $job)
             <a>
                 <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded lg">
-                    <div>{{ $job->employer->name }}</div>
+                    <div class="text-blue">{{ $job->employer->name }}</div>
                     <div>
                         <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                     </div>
