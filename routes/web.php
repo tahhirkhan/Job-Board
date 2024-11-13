84e5dcb2e807faabd0d2f459caa1d022558f197c
Tahir Khan <tahirpmsss.hitcse2020@gmail.com>
2024-11-13 17:00:49 +0530

    Updated routes file

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::view('/contact', 'contact');
 
 
 Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
 Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware('auth', 'can:edit-job,job');
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware(['auth', 'can:edit-job,job']);
+Route::patch('/jobs/{job}', [JobController::class, 'update'])->middleware(['auth', 'can:edit-job,job']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware(['auth', 'can:edit-job,job']);
 
 // Route::resource('jobs', JobController::class);
 
@@ -32,6 +32,6 @@ Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
